2a9d898c7b6a6756b77e492aeab7e37c7bf64767
jucian0 <juciano@outlook.com.br>
2021-05-11 08:59:51 -0300

    fix git url

--- src/config/index.js
+++ src/config/index.js
@@ -5,33 +5,29 @@ import fav from '../../public/green_logo.svg';
 import Head from 'next/head'
 
 export default {
-  repository: 'https://github.com/jucian0/next-lib-doc-starter',
-  docsRepository: 'https://github.com/jucian0/next-lib-doc-starter',
+  repository: 'https://github.com/use-form/use-form',
+  docsRepository: 'https://github.com/use-form/docs',
   branch: 'master',
-  siteName:'Next lib starter',
-  siteSlogan:'A starter project for library documentation made with nextjs.',
+  siteName: 'useForm',
+  siteSlogan: 'useFom provides a way to create complex forms easily.',
   path: '/',
-  titleSuffix: ' – Next lib starter',
+  titleSuffix: ' – useForm',
   nextLinks: true,
   prevLinks: true,
-  navLinks:[{
-     label:'Docs',
-     path:'/docs',
-  },
-  {
-     label:'Blog',
-     path:'/blog'
+  navLinks: [{
+    label: 'Docs',
+    path: '/docs',
   }
-],
+  ],
   defaultMenuCollapsed: false,
   font: true,
   footer: true,
-  footerText: 'MIT 2020 © Next lib starter.',
+  footerText: 'MIT 2020 © useForm.',
   footerEditOnGitHubLink: true,
   logo: (
     <React.Fragment>
       <img src={logo} alt="logo" />
-      <span className="mr-2 font-extrabold hidden md:inline">Next lib starter</span>
+      <span className="mr-2 font-extrabold hidden md:inline">useForm</span>
     </React.Fragment>
   ),
   head: (
@@ -40,12 +36,12 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Next lib starter: A nextjs doc stater template" />
+      <meta name="description" content="useForm: A nextjs doc stater template" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@juciano_barbosa" />
-      <meta property="og:title" content="Next lib starter: A nextjs doc stater template" />
-      <meta property="og:description" content="Next lib starter: A nextjs doc stater template" />
-      <meta name="apple-mobile-web-app-title" content="Next lib starter" />
+      <meta property="og:title" content="useForm: A nextjs doc stater template" />
+      <meta property="og:description" content="useForm: A nextjs doc stater template" />
+      <meta name="apple-mobile-web-app-title" content="useForm" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       <meta name="twitter:image" content={lib_logo} />
       <link rel="shortcut icon" href={fav} />
