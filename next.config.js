0a2a50cd8ba702fe431b93d3b090d3294d89c42e
Barbosa <barbosa@Barbosas-iMac.local>
2021-07-22 16:49:08 -0300

    remove withBlog plugin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images')
-const withBlog = require('next-mdx-blog');
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
 })
 
-module.exports = withPlugins([withMDX, withBlog, withImages], {
+module.exports = withPlugins([withMDX,  withImages], {
   pageExtensions: ['js', 'jsx', 'md', 'mdx']
 });
